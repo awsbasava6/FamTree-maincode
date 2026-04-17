78ba5fcb4e3349ec083937ad2d1688f0fe5364d2
root <root@ip-172-31-27-105.ec2.internal>
2026-04-17 04:37:17 +0000

    added backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "banking-terraform-state"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile = "terraform-lock"
+    use_lockfile = "true"
   }
 }
 
